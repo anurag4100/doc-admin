33d87b5882d8d9b24f4212b1098d5e20b23e072a
anurag4100 <anuragbharatpandey@gmail.com>
2022-04-30 13:54:07 +0530

    renamed static folder for aws compatibility

--- src/components/dashboard/latest-products.js
+++ src/components/dashboard/latest-products.js
@@ -1,5 +1,5 @@
-import { formatDistanceToNow, subHours } from 'date-fns';
-import { v4 as uuid } from 'uuid';
+import { formatDistanceToNow, subHours } from "date-fns";
+import { v4 as uuid } from "uuid";
 import {
   Box,
   Button,
@@ -10,64 +10,58 @@ import {
   List,
   ListItem,
   ListItemAvatar,
-  ListItemText
-} from '@mui/material';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+  ListItemText,
+} from "@mui/material";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const products = [
   {
     id: uuid(),
-    name: 'Dropbox',
-    imageUrl: '/static/images/products/product_1.png',
-    updatedAt: subHours(Date.now(), 2)
+    name: "Dropbox",
+    imageUrl: "/statics/images/products/product_1.png",
+    updatedAt: subHours(Date.now(), 2),
   },
   {
     id: uuid(),
-    name: 'Medium Corporation',
-    imageUrl: '/static/images/products/product_2.png',
-    updatedAt: subHours(Date.now(), 2)
+    name: "Medium Corporation",
+    imageUrl: "/statics/images/products/product_2.png",
+    updatedAt: subHours(Date.now(), 2),
   },
   {
     id: uuid(),
-    name: 'Slack',
-    imageUrl: '/static/images/products/product_3.png',
-    updatedAt: subHours(Date.now(), 3)
+    name: "Slack",
+    imageUrl: "/statics/images/products/product_3.png",
+    updatedAt: subHours(Date.now(), 3),
   },
   {
     id: uuid(),
-    name: 'Lyft',
-    imageUrl: '/static/images/products/product_4.png',
-    updatedAt: subHours(Date.now(), 5)
+    name: "Lyft",
+    imageUrl: "/statics/images/products/product_4.png",
+    updatedAt: subHours(Date.now(), 5),
   },
   {
     id: uuid(),
-    name: 'GitHub',
-    imageUrl: '/static/images/products/product_5.png',
-    updatedAt: subHours(Date.now(), 9)
-  }
+    name: "GitHub",
+    imageUrl: "/statics/images/products/product_5.png",
+    updatedAt: subHours(Date.now(), 9),
+  },
 ];
 
 export const LatestProducts = (props) => (
   <Card {...props}>
-    <CardHeader
-      subtitle={`${products.length} in total`}
-      title="Latest Products"
-    />
+    <CardHeader subtitle={`${products.length} in total`} title="Latest Products" />
     <Divider />
     <List>
       {products.map((product, i) => (
-        <ListItem
-          divider={i < products.length - 1}
-          key={product.id}
-        >
+        <ListItem divider={i < products.length - 1} key={product.id}>
           <ListItemAvatar>
             <img
               alt={product.name}
               src={product.imageUrl}
               style={{
                 height: 48,
-                width: 48
+                width: 48,
               }}
             />
           </ListItemAvatar>
@@ -75,10 +69,7 @@ export const LatestProducts = (props) => (
             primary={product.name}
             secondary={`Updated ${formatDistanceToNow(product.updatedAt)}`}
           />
-          <IconButton
-            edge="end"
-            size="small"
-          >
+          <IconButton edge="end" size="small">
             <MoreVertIcon />
           </IconButton>
         </ListItem>
@@ -87,17 +78,12 @@ export const LatestProducts = (props) => (
     <Divider />
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        p: 2
+        display: "flex",
+        justifyContent: "flex-end",
+        p: 2,
       }}
     >
-      <Button
-        color="primary"
-        endIcon={<ArrowRightIcon />}
-        size="small"
-        variant="text"
-      >
+      <Button color="primary" endIcon={<ArrowRightIcon />} size="small" variant="text">
         View all
       </Button>
     </Box>
